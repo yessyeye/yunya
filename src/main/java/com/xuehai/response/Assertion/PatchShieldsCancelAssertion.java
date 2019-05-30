0ed127d45b4b86999d26930192fc3894d394a987
罗鹏 <17681878652@100fen.cn>
2019-05-30 20:13:14 +0800

    新增匹配取消便签屏蔽名单、批量取消禁言

---
+++ src/main/java/com/xuehai/response/Assertion/PatchShieldsCancelAssertion.java
@@ -0,0 +1,24 @@
+package com.xuehai.response.Assertion;
+
+import com.alibaba.fastjson.JSONObject;
+import com.xuehai.base.AssertHandler;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+/**
+ * @Author slyart
+ * @create 2019/5/30 7:56 PM
+ */
+public class PatchShieldsCancelAssertion extends AssertHandler {
+    @Override
+    public String getExpectResult() {
+        return null;
+    }
+
+    @Override
+    public void execAssertion(String responseJson) {
+        JSONObject assertJson = JSONObject.parseObject(responseJson);
+        int code = assertJson.getIntValue("responseCode");
+        assertEquals(code, 204);
+    }
+}
---
+++ src/main/java/com/xuehai/response/testCase/Teacher/PatchShieldsCancel.java
@@ -0,0 +1,50 @@
+package com.xuehai.response.testCase.Teacher;
+
+import com.xuehai.base.AssertHandler;
+import com.xuehai.base.BaseTest;
+import com.xuehai.base.Log;
+import com.xuehai.model.Entity;
+import com.xuehai.response.Assertion.PatchShieldsCancelAssertion;
+import com.xuehai.util.OperateEntity;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ * @Author slyart
+ * @create 2019/5/30 7:12 PM
+ */
+public class PatchShieldsCancel extends BaseTest {
+    Log logger = new Log(PatchShieldsCancel.class);
+    private static HashMap<String, AssertHandler> assertMap = new HashMap<String, AssertHandler>();
+
+    static {
+        assertMap.put("first", new PatchShieldsCancelAssertion());
+    }
+
+    @BeforeSuite
+    public void beforeSuite(ITestContext context) {
+        super.setUpBeforeSuite(context);
+    }
+
+    @BeforeClass
+    @Parameters({"testCasePath1"})
+    public void beforeClass(@Optional("src/test/resources/case.xml") String testCasePath) {
+        super.setUpBeforeClass(testCasePath);
+    }
+
+    @Test(dataProvider = "data", description = "批量取消禁言")
+    public void action(Entity entity) {
+        //获取加签名后的entity中最新queryString
+        OperateEntity.getEntityValue(entity);
+        super.execute(entity, assertMap);
+
+    }
+
+    @DataProvider(name = "data")
+    public Iterator<Object[]> initData() {
+        return super.getDataIterator();
+    }
+}
